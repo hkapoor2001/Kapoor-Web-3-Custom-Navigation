8a4832cc15275e7c114807d971f63a40883b9f38
hkapoor2001 <hkapoor2001@gmail.com>
2022-02-10 15:09:34 -0500

    Fixed Transform Origin for nav div

--- dev/js/divAnimation.js
+++ dev/js/divAnimation.js
@@ -1,7 +1,7 @@
 import {gsap} from "gsap";
 gsap.set(".navdiv", {scaleY:0})
-// gsap.set(".navdiv",{transformOrigin:bottom,center})
+gsap.set(".navdiv",{transformOrigin: "center top"})
 
 export const divAnimation = new gsap.timeline({paused:true});
 
-divAnimation.to(".navdiv", {scaleY:-1, duration:.3, stagger:-.05, ease: "linear"})
+divAnimation.to(".navdiv", {scaleY:1, duration:.3, stagger:-.05, ease: "linear"})
